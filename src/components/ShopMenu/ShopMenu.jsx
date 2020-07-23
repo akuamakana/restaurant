c0c797b8495565ee6dc6770ccbce76eb5d0bce5e
akuamakana <akuamakana@gmail.com>
2020-07-22 19:50:05 -0700

    Editing ShopMenu

--- src/components/ShopMenu/ShopMenu.jsx
+++ src/components/ShopMenu/ShopMenu.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
-import ShopMenuItem from '../ShopMenuItem/ShopMenuItem'
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import ShopMenuItem from '../ShopMenuItem/ShopMenuItem';
+import menuItems from '../../resources/menuItems.json';
 
 function ShopMenu() {
+  const getCategories = () => {
+		let categories = menuItems.map((item) => item.category);
+		categories = categories.filter((category, i, uniqueCategories) => uniqueCategories.indexOf(category) === i)
+		return categories
+	};
+
+  console.log(getCategories());
+
   return (
-    <Table striped>
-      <tbody>
-        <ShopMenuItem />
-        <ShopMenuItem />
-        <ShopMenuItem />
-      </tbody>
-    </Table>
+    <Tabs defaultActiveKey="starters">
+      <Tab eventKey="starters" title="Starters">
+        <Table striped>
+          <tbody>
+            <ShopMenuItem />
+            <ShopMenuItem />
+            <ShopMenuItem />
+          </tbody>
+        </Table>
+      </Tab>
+    </Tabs>
   );
 }
 
